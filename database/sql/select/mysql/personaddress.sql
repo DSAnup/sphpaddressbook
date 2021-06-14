73328c5555261415aec8838cc8eeab7d4338ea98
DSAnup <anup12.m@gmail.com>
2021-06-14 17:59:51 +0600

    Left join personaddress with person & country table

--- database/sql/select/mysql/personaddress.sql
+++ database/sql/select/mysql/personaddress.sql
@@ -1,6 +1,9 @@
 SELECT			{ALIAS}.*, 
 				CONCAT({ALIAS}.PersonAddressType, '') AS {ENTITY}LookupCaption, 
+				CONCAT(P.PersonFirstName, '', P.PersonLastName) AS PersonName,
+				C.CountryName,
 				'' AS _Other
 
 FROM			{PREFIX}{NAME} AS {ALIAS}
-	/*LEFT JOIN		X AS Y ON Y.YID = {ALIAS}.YID*/
+	LEFT JOIN	{PREFIX}person	 AS P ON P.PersonID = {ALIAS}.PersonID
+	LEFT JOIN	sphp_country	 AS C ON C.CountryID = {ALIAS}.CountryID
